b746f9ca6a6c8573cd90c456f637a96b0c10ada7
ctuni <c.tuni.dominguez@gmail.com>
2023-08-25 14:07:30 +0200

    added script to quickly test pipeline's branches

--- test_pipeline.sh
+++ test_pipeline.sh
@@ -6,4 +6,4 @@ read user_input
 
 rm -rf test_outdir/
 # Use the input as a variable
-nextflow run Flomics/rnaseq -c /test_dir/test.config -params-file /test_dir/test_params.yml -r $user_input --outdir test_outdir/
+nextflow run Flomics/rnaseq -c ~/test_dir/test.config -params-file ~/test_dir/test_params.yml -r $user_input --outdir test_outdir/
