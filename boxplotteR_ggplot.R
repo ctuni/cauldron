b929eb49b1dffd83cf66e5a5d3f54a3723142940
ctuni <c.tuni.dominguez@gmail.com>
2023-11-15 14:54:06 +0100

    things

--- boxplotteR_ggplot.R
+++ boxplotteR_ggplot.R
@@ -16,14 +16,16 @@ filtered_data <- data[, selected_columns]
 # Create a list to store ggplot objects for each column
 ggplot_objects <- lapply(column_names, function(col_name) {
   ggplot(filtered_data, aes(x = dataset, y = .data[[col_name]], fill = dataset)) +
-    geom_boxplot(alpha = 0.7, position = position_dodge(width = 0.75)) +
-    geom_point(aes(y = .data[[col_name]]), 
-               position = position_jitterdodge(dodge.width = 0.75), 
-               shape = 16, size = 3, color = "black") +
+    geom_boxplot(alpha = 0.7, position = position_dodge(width = 0.75), outlier.shape = NA ) +
+    #geom_point(aes(y = .data[[col_name]]), 
+               #position = position_jitterdodge(dodge.width = 0.75), 
+               #shape = 16, size = 3, color = "black") +
     labs(title = paste("Boxplot for", col_name, "by Dataset"),
          x = "Dataset", y = col_name) +
     theme_minimal() +
+    theme(axis.text.x = element_text(angle = 45))+
     scale_fill_manual(values = rainbow(length(unique(filtered_data$dataset))))
+
 })
 
 # Save individual ggplot boxplots as JPEG images
